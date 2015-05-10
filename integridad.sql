ab2b357c55aa3156765a986e721e174d113e4f1a
Edgar Mendoza <edgarmch@gmail.com>
2015-05-09 20:36:50 -0500

    segundo trigger pk

--- integridad.sql
+++ integridad.sql
@@ -8,9 +8,36 @@ declare @usuario int
 select @usuario =  id_usuario from inserted
 declare @veces int
 select @veces = count(*) from Usuarios where Usuarios.id_usuario=@usuario
+if(@usuario not like "USU[0-9][0-9]")
+	    begin
+		print "Identificador incorrecto"
+		ROLLBACK TRANSACTION
+	end	
 if ( @veces > 1)
     begin
         print "El usuario ya existe""
+        ROLLBACK TRANSACTION
+    end
+go
+
+
+------2do trigger
+
+create trigger pkcursos on Cursos for insert as
+if( (select id_curso from inserted) in (null))
+    begin
+        ROLLBACK TRANSACTION
+        print "La clave no puede ser nula""
+    end
+ 
+declare @clave varchar(5)
+select @clave =  id_curso from inserted
+declare @veces int
+select @veces = count(*) from Cursos where Cursos.id_curso=@clave
+ 
+if ( @veces > 1)
+    begin
+        print "La clave ya existe"
         ROLLBACK TRANSACTION
     end
 go
